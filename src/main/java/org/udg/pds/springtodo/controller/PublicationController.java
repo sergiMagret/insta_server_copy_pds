67963c31341b15b2b9d371be07bcc15591f4b6dd
ainhoa <u1949100@campus.udg.edu>
2020-04-01 02:31:55 +0200

    Afegir Publicacio

--- src/main/java/org/udg/pds/springtodo/controller/PublicationController.java
+++ src/main/java/org/udg/pds/springtodo/controller/PublicationController.java
@@ -1,26 +1,33 @@
 package org.udg.pds.springtodo.controller;
 
-
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.udg.pds.springtodo.controller.exceptions.ControllerException;
 import org.udg.pds.springtodo.controller.exceptions.ServiceException;
-import org.udg.pds.springtodo.entity.Publication;
+import org.udg.pds.springtodo.entity.*;
+import org.udg.pds.springtodo.service.GroupService;
 import org.udg.pds.springtodo.service.PublicationService;
-import org.udg.pds.springtodo.entity.Views;
-
+import org.udg.pds.springtodo.service.UserService;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
-import java.util.List;
+import java.text.SimpleDateFormat;
 import java.util.Optional;
 
 @RequestMapping(path="/publications")
 @RestController
 public class PublicationController  extends BaseController{
+    public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
     @Autowired
     PublicationService publicationService;
+    @Autowired
+    UserService userService;
 
     @GetMapping
     @JsonView(Views.Public.class)
@@ -41,5 +48,19 @@ public class PublicationController  extends BaseController{
          }
          return op.get();
     }
+
+
+    @PostMapping (consumes = "application/json")
+    @JsonView(Views.Private.class)
+    public Publication postPublication (HttpSession session,@Valid @RequestBody String descripcion){
+        Publication p = new Publication("https://www.dailymoss.com/wp-content/uploads/2019/08/funny-profile-pic59.jpg",descripcion, new Date(2020, Calendar.APRIL, 28));
+        Long loggedUserId = getLoggedUser(session);
+        User u = userService.getUserProfile(loggedUserId);
+        p.setUser(u);
+        u.addPublication(p);
+        publicationService.addPublication(p);
+        return p;
+    }
+
 }
 
